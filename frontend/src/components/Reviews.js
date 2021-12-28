21b3c2306e64bb4794464fe83a7c95058a6bc2c4
Burak Özkan <burak.ozkan@path.com.tr>
2021-12-29 00:02:02 +0300

    add review component

---
+++ frontend/src/components/Reviews.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import StarRatings from './StarRatings'
+
+const Reviews = () => {
+  return (
+    <div className="row row-cols-3 mb-2">
+      <div className="card text-white bg-primary mb-3 mr-4" style={{ maxWidth: "30%" }}>
+        <div className="card-header d-flex justify-content-between">
+          <span>Joan</span>
+          <span>
+            <StarRatings rating={3} />
+          </span>
+        </div>
+        <div className="card-body">
+          <p className="card-text">This restaurant was awesome</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Reviews
